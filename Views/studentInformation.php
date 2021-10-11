b1afdf555160a10d6b7c6337a348c07ce9cbd1d5
paola <chiappettapaola@gmail.com>
2021-10-11 18:52:45 -0300

    StudentInformation View

--- Views/studentInformation.php
+++ Views/studentInformation.php
@@ -13,15 +13,21 @@
             <p>¡Hola <?php echo " " . $loggedUser->getFirstName() . "!"?></p>
         </div>
     </section> 
-    <section>
-        <?php
-        echo "Legajo N°: " . $loggedUser->getFileNumber() . "<br>";
-        echo "Nombre completo: " . $loggedUser->getFirstName() . " " . $loggedUser->getLastName() . "<br>";
-        echo "DNI N°: " . $loggedUser->getDni() . "<br>";
-        echo "Fecha de nacimiento: " . $loggedUser->getBirthDate() . "<br>";
-        echo "Email: " . $loggedUser->getEmail() . "<br>";
-        echo "Teléfono: " . $loggedUser->getPhoneNumber() . "<br>";
-        ?>
+    <section class="studentInfo">
+        <legend>Mis datos</legend>
+        <p class="data">Legajo</p> 
+        <p class="dataAnswer"><?php echo $loggedUser->getFileNumber()?></p>
+        <p class="data">Nombre completo</p> 
+        <p class="dataAnswer"><?php echo $loggedUser->getFirstName() . " " . $loggedUser->getLastName()?></p>
+        <p class="data">DNI</p> 
+        <p class="dataAnswer"><?php echo $loggedUser->getDni()?></p>
+        <p class="data">Fecha de nacimiento</p> 
+        <p class="dataAnswer"><?php echo $loggedUser->getBirthDate()?></p>
+        <p class="data">Email</p> 
+        <p class="dataAnswer"><?php echo $loggedUser->getEmail()?></p>
+        <p class="data">Teléfono</p> 
+        <p class="dataAnswer"><?php echo $loggedUser->getPhoneNumber()?></p>
+        <p class="data"><br></p>
     </section>     
   
 </main>
