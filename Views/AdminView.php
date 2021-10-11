c218a2580c7ee03085cc89204f06a45c6173287e
Gazaba Ávila Leonardo Damián <locusaa1@gmail.com>
2021-10-11 19:48:57 -0300

    Css test

--- Views/AdminView.php
+++ Views/AdminView.php
@@ -3,14 +3,16 @@ require_once('title.php');
 require_once('nav.php');
 ?>
     <main class="">
-        <section class="ourMision-Bg">
+        <nav class="adminNav">
+
+        </nav>
+        <!--<section class="ourMision-Bg">
             <div class="ourMision">
                 <p>Weolcome</p>
             </div>
-        </section>
-        <a href="#">MyAccount</a>
-        <a href="<?php echo FRONT_ROOT ?>Admin/enterpriseList">Enterprises List</a>
-        <a href="#">Add new enterprise</a>
+        </section>-->
+        <?php require_once('adminAside.php'); ?>
+
     </main>
 <?php
 require_once('companies.php');
---
+++ Views/adminAside.php
@@ -0,0 +1,6 @@
+<aside class="adminAside">
+    <a class="adminLink" href="#">MyAccount</a><br>
+    <a class="adminLink" href="<?php echo FRONT_ROOT ?>Admin/enterpriseList">Enterprises List</a><br>
+    <a class="adminLink" href="#">Add new enterprise</a><br>
+</aside>
+
