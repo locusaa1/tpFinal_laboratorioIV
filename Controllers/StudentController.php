4327df84fe12ef9f5cccedb1284e10002130e709
paola <chiappettapaola@gmail.com>
2021-10-23 09:57:06 -0300

    CheckEmail function modified (it generates a new user is it is not in the DB)

--- Controllers/StudentController.php
+++ Controllers/StudentController.php
@@ -3,6 +3,8 @@
 namespace Controllers;
 
 use DAO\StudentDAO as StudentDAO;
+use Models\User as User;
+use DAO\UserDB_DAO as UserDB_DAO;
 
 
 class StudentController
@@ -19,23 +21,38 @@ class StudentController
     to the studentView. If the student is not active, shows a rejection message.
     If the email is not on the student api, shows another rejection message. */
     
-    public function CheckEmail($email)
+    public function CheckEmail($email, $user, $password)
     {
 
         $student = $this->studentDAO->GetByEmail($email);
 
         if ($student) {
             if($student->getActive()){
-                $_SESSION ['student'] = $student;
+                if($user){
+                    $_SESSION ['user'] = $user;
                 require_once(VIEWS_PATH . "studentView.php");
+                }else{
+                    
+                    $user = new User();
+                    $user->setEmail($email);
+                    $user->setPassword($password);
+                    $user->setName($name);
+                    $user->setUserType("student");
+
+                    $userDao = new UserDB_DAO();
+                    $userDao->add($user);
+
+                    require_once(VIEWS_PATH . "studentView.php");
+                }
+                
             }else{
-                $_GET['notActive'] = 1;
-                require_once(VIEWS_PATH . "login.php");
+                $_GET['notActiveStudent'] = 1;
+                require_once(VIEWS_PATH . "logInUser.php");
             }
             
         } else {
-            $_GET['emailInvalid'] = 1;
-            require_once(VIEWS_PATH . "login.php");
+            $_GET['emailInvalidStudent'] = 1;
+            require_once(VIEWS_PATH . "logInUser.php");
         }
 
     }
