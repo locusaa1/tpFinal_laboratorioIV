bcaf718d2c2d3f61a9c705684a872ec3a6c561ef
Gazaba Ávila Leonardo Damián <locusaa1@gmail.com>
2021-10-24 15:36:30 -0300

    Update JobOfferDB_DAO.php
    Added the getAll() function into the jobOfferDB_DAO

--- DAO/JobOfferDB_DAO.php
+++ DAO/JobOfferDB_DAO.php
@@ -4,6 +4,7 @@
 namespace DAO;
 
 use Models\JobOffer as JobOffer;
+use DAO\Connection as Connection;
 use Exception as Exception;
 
 class JobOfferDB_DAO
@@ -40,5 +41,38 @@ class JobOfferDB_DAO
         }
     }
 
+    public function getAll()
+    {
+        try {
 
+            $jobOfferList = array();
+
+            $query = "select * from " . $this->tableName . ";";
+
+            $this->connection = Connection::GetInstance();
+
+            $resultSet = $this->connection->Execute($query);
+
+            foreach ($resultSet as $row) {
+
+                $jobOffer = new JobOffer();
+                $jobOffer->setIdJobOffer($row['id_job_offer']);
+                $jobOffer->setIdJobPosition($row['id_job_position']);
+                $jobOffer->setIdEnterprise($row['id_enterprise']);
+                $jobOffer->setIdUser($row['id_user']);
+                $jobOffer->setStartDate($row['start_date']);
+                $jobOffer->setLimitDate($row['limit_date']);
+                $jobOffer->setDescription($row['description']);
+                $jobOffer->setSalary($row['salary']);
+                $jobOffer->setResume($row['resume']);
+                $jobOffer->setCoverLetter($row['cover_letter']);
+                array_push($jobOfferList, $jobOffer);
+            }
+
+            return $jobOfferList;
+        } catch (Exception $exception) {
+
+            throw $exception;
+        }
+    }
 }
